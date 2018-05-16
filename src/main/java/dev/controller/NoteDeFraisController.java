24899d0a1a71c84f79c7951d5cbdeea85fc64d98
Kevin M <math_b@live.fr>
2018-05-16 19:02:56 +0200

    ajout de la suppression de frais

--- src/main/java/dev/controller/NoteDeFraisController.java
+++ src/main/java/dev/controller/NoteDeFraisController.java
@@ -10,6 +10,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -165,5 +166,15 @@ public class NoteDeFraisController {
 			return false;
 		}
 
+	}
+	
+	@DeleteMapping("frais/{id}")
+	public String supprimerFrais(@PathVariable Integer id) throws NoteDeFraisApiException {
+		// récupérer la ligne de frais
+		LigneDeFrais frais = ligneDeFraisRepo.findOne(id);
+		if(null == frais)
+			throw new NoteDeFraisApiException("La ligne de frais avec l'id " + id + " n'existe pas !");
+		ligneDeFraisRepo.delete(id);
+		return "Ligne de frais supprimées avec succès !";
 	}
 }
